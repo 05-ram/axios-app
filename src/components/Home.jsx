bab55f16fea5d8e3b5623eed4d7378e6331c5d53
05-ram <karthik98@gmail.com>
2024-10-08 18:16:55 +0530

    fetch records and display

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Button, Table } from 'react-bootstrap';
 
 const Home = () => {
-    return (
-        <div>Home Page</div>
-    )
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/users')
+      .then(res => setData(res.data))
+      .catch(err => console.log(err))
+  }, [])
+  return (
+    <div className='d-flex flex-column justify-content-center align-items-center bg-light vh-100'>
+      <h3>List Of Users</h3>
+      <div className="w-75 rounded bg-white border shadow p-4">
+        <Table striped>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Mobile</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              data.map((d, i) => (
+                <tr key={i} className='p-2'>
+                  <td>{d.id}</td>
+                  <td>{d.name}</td>
+                  <td>{d.email}</td>
+                  <td>{d.mobile}</td>
+                  <td>
+                    <div className="d-flex gap-2 align-items-center justify-content-center">
+                      <Button size='sm' variant='info'>Read</Button>
+                      <Button size='sm'>Edit</Button>
+                      <Button variant='danger' size='sm'>Delete</Button>
+                    </div>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </Table>
+      </div>
+    </div>
+  )
 }
 
 export default Home;
